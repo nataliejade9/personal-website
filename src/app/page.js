3933be63398edd3c889c8b3a9c70e37a243f979a
nataliejade9 <nataliejade9@gmail.com>
2023-11-30 17:18:32 +0000

    Update to profile banner - adding text and flex layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,17 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
-      <Image className={styles.profile} src="/natalie-office.jpg" alt="Portrait photo of Natalie Waites" width={4196} height={2241}/>
+      <div className='profile-banner'>
+        <div className='container-left'>
+          <Image className={styles.profile} src="/natalie-office.jpg" alt="Portrait photo of Natalie Waites" width={4196} height={2241}/>
+        </div>
+        <div className='container-right'>
+          <h1>Hey! I'm Natalie.</h1>
+          <h2>UI Designer</h2>
+          <p>I design digital products, that allow you to reach your goals.</p>
+        </div>
+     </div>
+      
       <ul className={styles.projects}>
         <li>
           <Link href="/project-one">Project One</Link>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import styles from './index.module.css'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h3 className={styles.logo}>Natalie Waites</h3>
+      <Image className={styles.logo} src="/Natalie-logo-v2.png" alt="Logo for Natalie Waites" width={968} height={968}/>
       <nav>
         <ul>
           <li>
