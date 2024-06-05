58cc118cb08f1ec07109ed8568cdcb89d3b36459
nataliejade9 <nataliejade9@gmail.com>
2024-06-05 17:11:00 +0100

    Link logo in header to homepage

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 import styles from './index.module.css'
 import Image from 'next/image'
 import Button from '../Button'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Image className={styles.logo} src="/Natalie-logo-v2.png" alt="Logo for Natalie Waites" width={968} height={968}/>
+      <Link href="/"><Image className={styles.logo} src="/Natalie-logo-v2.png" alt="Logo for Natalie Waites" width={968} height={968}/></Link>
       <nav>
         <ul>
           <li>
