bae95a3054b5768065285938e685336eb7513dd2
nataliejade9 <nataliejade9@gmail.com>
2024-06-29 14:27:46 +0100

    Edits to button component and change projects width

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,14 @@ import Link from 'next/link';
 import styles from './index.module.css'
 
 const Button = ({ type = "primary", label, href, onClick}) => {
-  const isMailLink = href.search("mailto:") === 0;
+  const isMailLink = href?href.search("mailto:") === 0:false;
   const className = `${styles.button} ${type === "primary" ? styles.buttonPrimary : styles.buttonSecondary}`;
-  if (isMailLink) {
+  if (!href) {
+    return (
+      <button className={className} onClick={onClick}>{label}</button>
+    )
+  }
+  else if (isMailLink) {
     return (
       <a className={className} href={href} onClick={onClick}>{label}</a>
     );
--- src/components/ContactButton/index.js
+++ src/components/ContactButton/index.js
@@ -3,11 +3,21 @@
 import Link from 'next/link';
 import styles from './index.module.css'
 import Button from '../Button';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const ContactButton = () => {
-    const onClick = useCallback (()=> {console.log("clicked")}, [])
-    return <Button type="primary" label="Let's Chat" href="mailto:natalie@superwatermelon.com?subject=Hello Natalie, I've been sent from your website" onClick={onClick}/>
+    const [clickState, setClickState] = useState(false)
+    const onClick = useCallback (()=> {
+        setClickState(true)
+    }, [])
+    if (clickState) {
+        return <Button type="secondary" label="natalie@superwatermelon.com"/>
+    }
+    else {
+        return <Button type="primary" label="Let's Chat" href="mailto:natalie@superwatermelon.com?subject=Hello Natalie, I've been sent from your website" onClick={onClick}/>
+    }
+    
 }
+
 
 export default ContactButton;
