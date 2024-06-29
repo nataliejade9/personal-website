2ae60e178f1af0c53b8d28b89c6dae16c07d0910
nataliejade9 <nataliejade9@gmail.com>
2024-06-29 15:58:35 +0100

    Add copied message

--- src/components/ContactButton/index.js
+++ src/components/ContactButton/index.js
@@ -7,17 +7,37 @@ import { useCallback, useState } from 'react';
 
 const ContactButton = () => {
     const [clickState, setClickState] = useState(false)
+    const [messageState, setMessageState] = useState(false)
     const onClick = useCallback (()=> {
         setClickState(true)
+        writeClipboardText("natalie@superwatermelon.com").then(()=> {
+            setMessageState(true)
+        })
     }, [])
     if (clickState) {
-        return <Button type="secondary" label="natalie@superwatermelon.com"/>
+        return (
+            <div className={styles.contactContainer}>
+                <Button type="secondary" label="natalie@superwatermelon.com"/>
+                {messageState && (
+                    <div className={styles.copied}>Copied!</div>
+                )}
+            </div>
+        )
+        
     }
     else {
         return <Button type="primary" label="Let's Chat" href="mailto:natalie@superwatermelon.com?subject=Hello Natalie, I've been sent from your website" onClick={onClick}/>
-    }
-    
+    } 
 }
+
+async function writeClipboardText(text) {
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  
 
 
 export default ContactButton;
