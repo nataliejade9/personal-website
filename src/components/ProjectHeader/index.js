09c10db5ee112cc7cea97ee73c87e03a6d703d8d
nataliejade9 <nataliejade9@gmail.com>
2024-09-27 16:36:54 +0100

    css edits for sierra

--- src/components/ProjectHeader/index.js
+++ src/components/ProjectHeader/index.js
@@ -4,14 +4,14 @@ import Image from 'next/image'
 const ProjectHeader = ({projectName, projectDescription, imageSource, imageAlt, imageWidth, imageHeight}) => {
     return (
         <div className={styles.projectHeader}>
-        <div className={styles.titleInfo}>
-            <h1>{projectName}</h1>
-            <h2>{projectDescription}</h2>
-        </div>
-        <div className={styles.projectHeaderImg}>
-            <Image className={styles.coverImg} src={imageSource} alt={imageAlt} width={imageWidth} height={imageHeight}/>
-        </div>
-    </div> 
+            <div className={styles.titleInfo}>
+                <h1>{projectName}</h1>
+                <h2>{projectDescription}</h2>
+            </div>
+            <div className={styles.projectHeaderImg}>
+                <Image className={styles.coverImg} src={imageSource} alt={imageAlt} width={imageWidth} height={imageHeight}/>
+            </div>
+        </div> 
     )
 }
 
