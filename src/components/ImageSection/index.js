489d651648d33e7854533378a3e9196b3c476d17
nataliejade9 <nataliejade9@gmail.com>
2024-10-09 15:37:38 +0100

    Add optional title to ImageSection

--- src/components/ImageSection/index.js
+++ src/components/ImageSection/index.js
@@ -1,9 +1,10 @@
 import styles from './index.module.css'
 import Image from 'next/image'
 
-const ImageSection = ({imageSource, imageAlt, imageWidth, imageHeight}) => {
+const ImageSection = ({imgTitle, imageSource, imageAlt, imageWidth, imageHeight}) => {
     return (
         <div className={styles.projectImg}>
+            {imgTitle && (<h2>{imgTitle}</h2>)}
             <Image className={styles.img} src={imageSource} alt={imageAlt} width={imageWidth} height={imageHeight}/>
         </div>
     )
