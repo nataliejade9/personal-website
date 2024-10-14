78426f0f39216b098f7ff99104097149e6aa608a
nataliejade9 <nataliejade9@gmail.com>
2024-10-14 16:22:56 +0100

    Add styling to case study link

--- src/components/CasestudyBanner/index.js
+++ src/components/CasestudyBanner/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const CasestudyBanner = ({}) => {
     return (
-        <h2 className={styles.promo}>Want a walk-through of this case study?{" "}<Link id='linkStyles' href="https://calendar.app.google/xjTKr9jayhcL5cP1A">Let's schedule a time</Link></h2>
+        <h2 className={styles.promo}>Want a walk-through of this case study?{" "}<Link href="https://calendar.app.google/xjTKr9jayhcL5cP1A" className={styles.linkStyle}>Let's schedule a time</Link></h2>
     )
 }
 
