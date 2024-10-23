bee86f15191affc5e76e2c587625b592891daae8
nataliejade9 <nataliejade9@gmail.com>
2024-10-23 17:30:02 +0100

    Image section for experiments

--- src/app/experiments/page.js
+++ src/app/experiments/page.js
@@ -1,6 +1,7 @@
 import styles from './page.module.css'
 import ProjectHeader from '@/components/ProjectHeader'
 import ExperimentSection from '@/components/ExperimentSection'
+import ImageSection from '@/components/ImageSection'
 
 export const metadata = {
   title: 'Experiments | Natalie Waites'
@@ -10,7 +11,14 @@ export default function ExperimentsPage() {
   return (
     <main className={styles.main}>
       <ProjectHeader projectName={"Experiments"} projectDescription={"Throughout my life I've loved playing around with my design skills in various different mediums."}  imageSource={"/images/experiments/hero-experiments.png"} imageAlt={"Coding project on laptop and mobile mock up"} imageWidth={3264} imageHeight={3500}/>
-      <ExperimentSection title={"Fashion Design & Construction"} /> 
+      <ExperimentSection 
+        title={"Fashion Design & Construction"} 
+        imageSection={
+          <>
+            <ImageSection/>
+          </>
+        }
+      /> 
       
       <div className={styles.uni}>
 
--- src/components/ExperimentSection/index.js
+++ src/components/ExperimentSection/index.js
@@ -1,11 +1,11 @@
 import ImageSection from '../ImageSection'
 import styles from './index.module.css'
 
-const ExperimentSection = ({title}) => {
+const ExperimentSection = ({title, imageSection}) => {
     return (
         <div className={styles.experimentContent}>
             <h2>{title}</h2>
-            <><ImageSection/></>
+            <>{imageSection}</>
         </div>
     )
 }
