7bfe0ce9652299339006d38d21efd08d1ae4f2b1
nataliejade9 <nataliejade9@gmail.com>
2024-10-23 13:44:46 +0100

    Umuntu content upload

--- src/app/umuntu/page.js
+++ src/app/umuntu/page.js
@@ -17,7 +17,7 @@ export default function ProjectPage() {
       <ProjectHeader projectName={"Umuntu"} projectDescription={"A central place for the Nigerian Diaspora to connect and share information."}  imageSource={"/images/umuntu/umuntu-header.png"} imageAlt={"Umuntu homepage displayed on laptop and mobile"} imageWidth={3400} imageHeight={3000}/>
    
       <OverviewSection
-          overviewSummary={"A platform that allows users to be able to connect with the social and political life of their countries back home, empowering their voices, as well as contributing to their communities of origin."} 
+          overviewSummary={"A platform that allows users to be able to connect with the social and political life of their countries back home, empowering their voices, as well as contributing to their communities of origin. I used the double diamond approach for this project, as there were a lot of elements within the brief and I wanted to keep the project focused."} 
           roleTitle={"Role: Product Design (UX & UI)"} 
           roleText={"UX design, research & user testing, UI design of responsive web platform, high-fidelity prototyping"} 
           teamText={"Natalie Waites (solo project)"} 
@@ -31,7 +31,7 @@ export default function ProjectPage() {
           <TitleSection title={"The Brief"} information={"The initial brief was quite broad: 'A platform that empowers the African diaspora to engage with and impact the socio-political landscape of African countries through news, policy tracking, funding, and advocacy. Focused on developing a web platform with these functionalities.' After interviewing the client we agreed to define the brief further by understanding more about the business and conducting target user research."}/>
           <ImageSection imgTitle={"Understanding the business: Lean canvas and competitor analysis"} imageSource={"/images/umuntu/Umuntu-lean-canvas.png"} imageAlt={"Lean canvas diagram for Umuntu business"} imageWidth={7072} imageHeight={5384}/>
           <Image className={styles.onlyImg} src="/images/umuntu/Umuntu-competitor.png" alt="Competitor analysis for Umuntu business" width={7776} height={6690}/>
-          <TitleSection title={"User survey results"} information={"Once I understood more about the business, I felt it was important to get out a target user survey to understand the market, get initial feedback on the problems presented and how user's currently solve them. For this project in particular it was important for me to allow the product to be driven by the target audience and their experience."}/>
+          <TitleSection title={"User Survey Results"} information={"Once I understood more about the business, I felt it was important to get out a target user survey to understand the market, get initial feedback on the problems presented and how user's currently solve them. For this project in particular it was important for me to allow the product to be driven by the target audience and their experience."}/>
           <div className={styles.stats}>
             <h3>The user survey guided the product direction and decision making</h3>
             <Image className={styles.onlyImg} src="/images/umuntu/mobile-first-stats.png" alt="Pie chart showing users prefer using mobile by 75%" width={2760} height={1552}/>
@@ -42,14 +42,20 @@ export default function ProjectPage() {
           <TitleSection title={"User Interviews"} information={"I was able to arrange a more in-depth interview with three people who participated in the survey. This was super valuable to the project as not only did I receive more information on the problem and their solutions but I was able to listen to their experience and understand the bigger picture. It was important for me to empathise and understand the situation user's were in as best I could to build a product that would suit them."}/>
           <ImageSection imgTitle={"Key quotes from the interviews and affinity map of everything discussed"} imageSource={"/images/umuntu/user-quotes.png"} imageAlt={"Quotes from user interview"} imageWidth={2760} imageHeight={1552}/>  
           <Image className={styles.onlyImg} src="/images/umuntu/Umuntu-affinityMap.png" alt="Affinity map for the three user interviews" width={14306} height={9818}/>
-          <TitleSection title={"Defining interview information"} information={"From the interviews I made an experience map to understand the problems faced by the user and where Umuntu could fit in that picture. Then, I used the affinity map, to pull out the key themes discussed and would later use these to form the How Might We statements."}/>
-          <ImageSection imageSource={"/images/umuntu/Umuntu-experience-map.png"} imageAlt={"Experience map for users"} imageWidth={6976} imageHeight={4396} imgDescription={"Within this experience map we have identified where Umuntu could sit. Fulfilling users' needs, where they are currently underserved."}/>
-          <Image className={styles.onlyImg} src="/images/umuntu/key-themes.png" alt="Key themes from the user interviews" width={2754} height={1552}/>
+          <TitleSection title={"Defining Interview Information"} information={"From the affinity map, I was able to pull out the key themes discussed and would later use these to form the How Might We statements. I then made an experience map to understand the problems faced by user's and how they played out. I plotted where Umuntu could fit in that experience."}/>
+          <ImageSection imageSource={"/images/umuntu/key-themes.png"} imageAlt={"Key themes from the user interviews"} imageWidth={2754} imageHeight={1552}/>
+          <Image className={styles.onlyImg} src="/images/umuntu/Umuntu-experience-map.png" alt="Experience map for users" width={6976} height={4396}/>
+          <TitleSection title={"Design Ideation"} information={"Going into the design stages I wanted to make sure I was clear on the key opportunities that Umuntu could fill in this space. From there I was able to build three 'How Might We' statements in order to direct the design process and use them to support ideation"}/>
+          <ImageSection imageSource={"/images/umuntu/opportunities.png"} imageAlt={"Opportunites indetified for Umuntu"} imageWidth={2758} imageHeight={1554}/>
+          <Image className={styles.onlyImg} src="/images/umuntu/HMW.png" alt="How Might We statements for Umuntu" width={2758} height={1554}/>
 
-          <TitleSection title={"Design Ideation"} information={""}/>
-          <ImageSection imageSource={"/images/umuntu/Umuntu-experience-map.png"} imageAlt={"Experience map for users"} imageWidth={6976} imageHeight={4396} imgDescription={"Within this experience map we have identified where Umuntu could sit. Fulfilling users' needs, where they are currently underserved."}/>
-          <Image className={styles.onlyImg} src="/images/umuntu/key-themes.png" alt="Key themes from the user interviews" width={2754} height={1552}/>
+          <TitleSection title={"Initial Design Stage"} information={"Going into the design stages I wanted to make sure I was clear on the key opportunities that Umuntu could fill in this space. From there I was able to build three 'How Might We' statements in order to direct the design process and use them to support ideation"}/>
+          <ImageSection imageSource={"/images/umuntu/opportunities.png"} imageAlt={"Opportunites indetified for Umuntu"} imageWidth={2758} imageHeight={1554}/>
+          <Image className={styles.onlyImg} src="/images/umuntu/HMW.png" alt="How Might We statements for Umuntu" width={2758} height={1554}/>
           
+
+
+
           <div className={styles.finalSection}>
             <Image className={styles.onlyImg} src="/images/finchair/Homepages.png" alt="Five homepage screenshots of the finchair app final designs" width={6000} height={3500}/> 
             <Image className={styles.onlyImg} src="/images/finchair/3-iphone-blue.png" alt="Three screenshots of the finchair app final desgins, the wallet, bills and rewards pages" width={4500} height={2800}/>
