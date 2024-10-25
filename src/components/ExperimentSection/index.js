7d9187df6ee6011ea153e095ccded1f416b232c7
nataliejade9 <nataliejade9@gmail.com>
2024-10-25 17:42:10 +0100

    Experiments page edits

--- src/components/ExperimentSection/index.js
+++ src/components/ExperimentSection/index.js
@@ -1,11 +1,11 @@
 import ImageSection from '../ImageSection'
 import styles from './index.module.css'
 
-const ExperimentSection = ({title, imageSection}) => {
+const ExperimentSection = ({title, imageCards}) => {
     return (
         <div className={styles.experimentContent}>
             <h2>{title}</h2>
-            <>{imageSection}</>
+            <>{imageCards}</>
         </div>
     )
 }
