a77b5064cdc27bc3bcb086abd7b06018d4492e67
Stuart Wakefield <stuart@superwatermelon.com>
2024-11-08 20:38:30 +0000

    Update Randomiser logic
    Does not select the same page twice in a row and
    does not select the page the user is currently on

--- src/components/Randomiser/index.js
+++ src/components/Randomiser/index.js
@@ -1,12 +1,26 @@
 'use client'
 import styles from '../Button/index.module.css'
 
-import { useCallback } from 'react'
-import { useRouter } from 'next/navigation'
+import {useCallback, useMemo, useState} from 'react'
+import {usePathname, useRouter} from 'next/navigation'
 
 export default function Randomiser({ links }) {
     const router = useRouter();
-    const randomIndex = Math.floor(Math.random() * links.length);
-    const onRandomise = useCallback(() => router.push(links[randomIndex]), [links, randomIndex, router]);
+    const currentLink = usePathname();
+    const [lastLink, setLastLink] = useState(null);
+    const activeLinks = useMemo(() => {
+      return links
+        .filter(link => currentLink !== link)
+        .filter(link => lastLink !== link);
+    }, [links, currentLink, lastLink]);
+    const onRandomise = useCallback(() => {
+      const link = activeLinks[randomIndexOf(activeLinks)];
+      router.push(link);
+      setLastLink(link);
+    }, [router, activeLinks, setLastLink]);
     return <a className={`${styles.button} ${styles.buttonSecondary}`} style={{cursor:"pointer"}} onClick={onRandomise}>Randomiser</a>
+}
+
+function randomIndexOf(links) {
+  return Math.floor(Math.random() * links.length);
 }
