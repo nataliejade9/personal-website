8202a3f9a6f2cc7fa08a6126616e5635c9c92673
nataliejade9 <nataliejade9@gmail.com>
2024-11-08 17:50:47 +0000

    Fix Umuntu images

--- src/app/umuntu/page.js
+++ src/app/umuntu/page.js
@@ -29,8 +29,8 @@ export default function ProjectPage() {
         <div className={styles.mainProject}>
           <Image className={styles.onlyImg} src="/images/umuntu/hero-img.png" alt="Umuntu home screen on laptop and iphone mockup screens" width={4500} height={2700}/>
           <TitleSection title={"The Brief"} information={"The initial brief was quite broad: 'A platform that empowers the African diaspora to engage with and impact the socio-political landscape of African countries through news, policy tracking, funding, and advocacy. Focused on developing a web platform with these functionalities.' After interviewing the client we agreed to define the brief further by understanding more about the business and conducting target user research."}/>
-          <ImageSection imgTitle={"Understanding the business: Lean canvas and competitor analysis"} imageSource={"/images/umuntu/umuntu-lean-canvas.png"} imageAlt={"Lean canvas diagram for Umuntu business"} imageWidth={7072} imageHeight={5384}/>
-          <Image className={styles.onlyImg} src="/images/umuntu/umuntu-competitor.png" alt="Competitor analysis for Umuntu business" width={7776} height={6690}/>
+          <ImageSection imgTitle={"Understanding the business: Lean canvas and competitor analysis"} imageSource={"/images/umuntu/umuntu-lean-canvas-1.png"} imageAlt={"Lean canvas diagram for Umuntu business"} imageWidth={7072} imageHeight={5384}/>
+          <Image className={styles.onlyImg} src="/images/umuntu/umuntu-competitor-analysis.png" alt="Competitor analysis for Umuntu business" width={7776} height={6690}/>
           <TitleSection title={"User Survey Results"} information={"Once I understood more about the business, I felt it was important to get out a target user survey to understand the market, get initial feedback on the problems presented and how user's currently solve them. For this project in particular it was important for me to allow the product to be driven by the target audience and their experience."}/>
           <div className={styles.stats}>
             <h3>The user survey guided the product direction and decision making</h3>
@@ -44,12 +44,12 @@ export default function ProjectPage() {
           <Image className={styles.onlyImg} src="/images/umuntu/umuntu-affinity-map.png" alt="Affinity map for the three user interviews" width={14306} height={9818}/>
           <TitleSection title={"Defining Interview Information"} information={"From the affinity map, I was able to pull out the key themes discussed and would later use these to form the How Might We statements. I then made an experience map to understand the problems faced by user's and how they played out. I plotted where Umuntu could fit in that experience."}/>
           <ImageSection imageSource={"/images/umuntu/key-themes.png"} imageAlt={"Key themes from the user interviews"} imageWidth={2754} imageHeight={1552}/>
-          <Image className={styles.onlyImg} src="/images/umuntu/umuntu-experience-map.png" alt="Experience map for users" width={6976} height={4396}/>
+          <Image className={styles.onlyImg} src="/images/umuntu/umuntu-experience.png" alt="Experience map for users" width={6976} height={4396}/>
           <TitleSection title={"Design Ideation"} information={"Going into the design stages I wanted to make sure I was clear on the key opportunities that Umuntu could fill in this space. From there I was able to build three 'How Might We' statements in order to direct the design process and use them to support ideation."}/>
           <ImageSection imageSource={"/images/umuntu/opportunities.png"} imageAlt={"Opportunities identified for Umuntu"} imageWidth={2758} imageHeight={1554}/>
-          <Image className={styles.onlyImg} src="/images/umuntu/hmw.png" alt="How Might We statements for Umuntu" width={2758} height={1554}/>
+          <Image className={styles.onlyImg} src="/images/umuntu/h-m-w.png" alt="How Might We statements for Umuntu" width={2758} height={1554}/>
           <TitleSection title={"Initial Design Phase"} information={"I started the design process by making a moodboard to get a feel for what direction we were going for. I incorporated ideas from user feedback as well as the logo provided by the client. I moved on to some low fidelity wireframes to experiment with different features within the app before jumping into Figma."}/>
-          <ImageSection imageSource={"/images/umuntu/umuntu-moodboard.png"} imageAlt={"Moodboard for Umuntu"} imageWidth={5530} imageHeight={3899}/>
+          <ImageSection imageSource={"/images/umuntu/umuntu-moodboard-1.png"} imageAlt={"Moodboard for Umuntu"} imageWidth={5530} imageHeight={3899}/>
           <Image className={styles.onlyImg} src="/images/umuntu/wireframe.png" alt="Low fidelity wireframes for umuntu features" width={3024} height={2366}/>
           <TitleSection title={"Final Prototype & Designs"} information={"I presented these final prototype and designs to the client and the user's who participated in the interviews. The client was happy that it fulfilled the brief and I was able to get some final testing and feedback from user's. Overall the feedback was positive but I've included suggested edits for future. I've also included the implementation roadmap I provided for the client."}/>
           <div className={styles.video}>
@@ -60,10 +60,10 @@ export default function ProjectPage() {
           </div>
           <div className={styles.finalSection}>
             <ImageSection imageSource={"/images/umuntu/home-screens.png"} imageAlt={"Umuntu home screens in three iphone mock ups"} imageWidth={4500} imageHeight={2600}/>
-            <Image className={styles.onlyImg} src="/images/umuntu/community.png" alt="Umuntu community screens in three iphone mock ups" width={4500} height={2500}/> 
-            <Image className={styles.onlyImg} src="/images/umuntu/social-projects.png" alt="Umuntu social projects page on iphone mock up" width={4500} height={2500}/>
-            <Image className={styles.onlyImg} src="/images/umuntu/email-2.0.png" alt="Umuntu monthly digest example on iphone mock up" width={4500} height={2500}/>
-            <Image className={styles.onlyImg} src="/images/umuntu/final-testing.png" alt="User testing feedback of final design" width={2758} height={1550}/>
+            <Image className={styles.onlyImg} src="/images/umuntu/umuntu-community.png" alt="Umuntu community screens in three iphone mock ups" width={4500} height={2500}/> 
+            <Image className={styles.onlyImg} src="/images/umuntu/umuntu-social-projects.png" alt="Umuntu social projects page on iphone mock up" width={4500} height={2500}/>
+            <Image className={styles.onlyImg} src="/images/umuntu/umuntu-email.png" alt="Umuntu monthly digest example on iphone mock up" width={4500} height={2500}/>
+            <Image className={styles.onlyImg} src="/images/umuntu/umuntu-final-testing.png" alt="User testing feedback of final design" width={2758} height={1550}/>
             <Image className={styles.onlyImg} src="/images/umuntu/roadmap.png" alt="Implementation roadmap" width={2756} height={1546}/>
           </div>  
           <ReviewSection 
