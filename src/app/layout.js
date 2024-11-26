4bc0e94ff96a010eadd6f832f49ef7036004b37a
nataliejade9 <nataliejade9@gmail.com>
2024-11-26 15:01:02 +0000

    Add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import './globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Contact from '../components/Contact'
+import Head from 'next/head'
+import Script from 'next/script'
 
 const font = Inter({ subsets: ['latin'], variable: '--inter-font' })
 const ralewayfont = Raleway ({subsets: ['latin'], variable: '--raleway-font' })
@@ -15,6 +17,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-1BGQMKBN7R"></Script>
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-1BGQMKBN7R');
+         `}
+        </Script>
+      </Head>
       <body className={`${font.variable} ${ralewayfont.variable}`}>
         <Header/>
         <div className='header-offset'>{children}</div>
