2d1fb8d8d8c42dac8d32b1310be5b59d7ecb8b7e
Stuart Wakefield <stuart@superwatermelon.com>
2024-12-17 17:56:39 +0000

    Rename to invalidate cache

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    localPatterns: [
+      {
+        pathname: '/**',
+        search: ''
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
