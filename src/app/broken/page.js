db3a64589eda28f7f31c12c148d9ccb131ac6559
Stuart Wakefield <stuart@superwatermelon.com>
2024-12-18 22:24:12 +0000

    Process broken images

--- src/app/broken/page.js
+++ src/app/broken/page.js
@@ -9,8 +9,8 @@ export const metadata = {
 export default function ProjectPage() {
   return (
     <main className={styles.main}>
-      <Image className={styles.onlyImg} src="/images/umuntu/umuntu-affinity-map.png" alt="Affinity map for the three user interviews" width={1920} height={1058}/>
-      <ImageSection imgTitle={"Survey Results"} imageSource={"/images/housebrain/research-results.png"} imageAlt={"User research results for survey conducted for HouseBrain App"} imageWidth={1472} imageHeight={1071}/>
+      <Image className={styles.onlyImg} src="/images/umuntu/umuntu-affinity-map.webp" alt="Affinity map for the three user interviews" width={1920} height={1058}/>
+      <ImageSection imgTitle={"Survey Results"} imageSource={"/images/housebrain/research-results.webp"} imageAlt={"User research results for survey conducted for HouseBrain App"} imageWidth={1472} imageHeight={1071}/>
     </main>
   )
 }
